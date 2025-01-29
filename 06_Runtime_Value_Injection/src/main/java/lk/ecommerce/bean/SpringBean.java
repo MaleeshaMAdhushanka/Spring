c06328582dcd3cc46ecd1ea22b63018325042338
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-01-29 11:50:36 +0530

    Run Value

---
+++ 06_Runtime_Value_Injection/src/main/java/lk/ecommerce/bean/SpringBean.java
@@ -0,0 +1,37 @@
+package lk.ecommerce.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+//using run wena welawe value ekka daganna oni nam @Value annotation eka use karanna
+@Component
+public class SpringBean {
+//    public SpringBean(){}
+//Argument wedima constructor eka thama ganne
+    //Prameter count eka weidma eka use kranne
+
+    @Autowired(required = false)
+    //menna meka daganna kiyala run time ekedi
+    public SpringBean(@Value("Maleesha") String name,
+                      @Value("1") int id) {
+
+        System.out.println("Spring Bean Object Created");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+    }
+
+    @Autowired(required = false)
+    public SpringBean(@Value("Maleesha") String name,
+                      @Value("1") int id,
+                      @Value("true") boolean b) {
+
+        System.out.println("Spring Bean Object Created");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("b: " + b);
+    }
+}
+
+
+
